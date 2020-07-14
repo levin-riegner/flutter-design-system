93b60159f46579fea8273014d478207b6444fdfd
Alex Queudot <alqueraf@gmail.com>
2020-07-14 14:51:41 +0200

    Added divider to list item

--- lib/views/ds_list_item_action.dart
+++ lib/views/ds_list_item_action.dart
@@ -4,45 +4,62 @@ import 'package:lr_design_system/theme/theme.dart';
 class DSListItemAction extends StatelessWidget {
   final String text;
   final DSListItemActionType type;
+  final bool bottomDivider;
   final Widget leading;
   final Function() onPressed;
 
-  DSListItemAction({@required this.text, @required this.type, @required this.onPressed, this.leading});
+  DSListItemAction({
+    @required this.text,
+    @required this.type,
+    @required this.onPressed,
+    this.leading,
+    this.bottomDivider,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressed,
-      child: SizedBox(
-        width: double.infinity,
-        height: ThemeProvider.theme.dimensions.listItemHeight,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: ThemeProvider.theme.spacing.l),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              if (leading != null)
-                Padding(
-                  padding: EdgeInsets.only(right: ThemeProvider.theme.spacing.s),
-                  child: leading,
-                ),
-              Expanded(
-                child: Text(
-                  text,
-                  style: ThemeProvider.theme.textStyles.body1.copyWith(
-                    color: type == DSListItemActionType.destructive ? ThemeProvider.theme.colors.primary : ThemeProvider.theme.colors.onBackground,
+    return Column(
+      children: <Widget>[
+        InkWell(
+          onTap: onPressed,
+          child: SizedBox(
+            width: double.infinity,
+            height: ThemeProvider.theme.dimensions.listItemHeightLarge,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: ThemeProvider.theme.spacing.l),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  if (leading != null)
+                    Padding(
+                      padding: EdgeInsets.only(right: ThemeProvider.theme.spacing.s),
+                      child: leading,
+                    ),
+                  Expanded(
+                    child: Text(
+                      text,
+                      style: ThemeProvider.theme.textStyles.body1.copyWith(
+                        color: type == DSListItemActionType.destructive
+                            ? ThemeProvider.theme.colors.primary
+                            : ThemeProvider.theme.colors.onBackground,
+                      ),
+                    ),
                   ),
-                ),
+                  if (type == DSListItemActionType.navigation)
+                    Icon(
+                      Icons.chevron_right,
+                      color: ThemeProvider.theme.colors.primary,
+                    ),
+                ],
               ),
-              if (type == DSListItemActionType.navigation)
-                Icon(
-                  Icons.chevron_right,
-                  color: ThemeProvider.theme.colors.primary,
-                ),
-            ],
+            ),
           ),
         ),
-      ),
+        Divider(
+          height: ThemeProvider.theme.dimensions.dividerHeight,
+          color: ThemeProvider.theme.colors.interaction,
+        ),
+      ],
     );
   }
 }
