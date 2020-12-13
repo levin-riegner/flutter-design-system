8bbb98b4fc434a5a9982935483f213eb327e0231
JTorrus <jtorrusgalindo@gmail.com>
2020-12-13 03:23:03 +0100

    Theme parsing testing with font fallback options

--- test/designsystem_test.dart
+++ test/designsystem_test.dart
@@ -13,14 +13,18 @@ void main() {
     expect(ThemeProvider.theme != null, true);
     expect(ThemeProvider.theme.colors.primary, Color(0xFF6200EE));
     expect(
-        ThemeProvider.theme.textStyles.h1,
-        TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 96,
-            letterSpacing: -1.5,
-            height: 96 / 96,
-            fontStyle: FontStyle.normal,
-            fontFamily: "roboto"));
+      ThemeProvider.theme.textStyles.h1,
+      TextStyle(
+        fontWeight: FontWeight.w300,
+        fontSize: 96,
+        letterSpacing: -1.5,
+        height: 96 / 96,
+        fontStyle: FontStyle.normal,
+        fontFamily: "roboto",
+        fontFamilyFallback: ["Roboto"],
+        package: "flutter",
+      ),
+    );
     expect(ThemeProvider.theme.dimensions.listItemHeight, 48.0);
     expect(ThemeProvider.theme.spacing.m, 16);
   });
