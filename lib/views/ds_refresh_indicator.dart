f89fd5e4033d9a7e61682083254460005421df57
JTorrus <jtorrusgalindo@gmail.com>
2021-04-12 21:44:45 +0200

    Added DSRefreshIndicator based on PlatformAwareWidget for multiplatform UIs

---
+++ lib/views/ds_refresh_indicator.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lr_design_system/utils/platform_aware_widget.dart';
+
+class DSRefreshIndicator
+    extends PlatformAwareWidget<CustomScrollView, CustomScrollView> {
+  final Future<void> Function() onRefresh;
+  final Widget child;
+  final Widget sliverAppBar;
+
+  DSRefreshIndicator({
+    @required this.onRefresh,
+    @required this.child,
+    @required this.sliverAppBar,
+  });
+
+  @override
+  CustomScrollView buildAndroidWidget(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        sliverAppBar,
+        SliverToBoxAdapter(
+          child: RefreshIndicator(
+            onRefresh: onRefresh,
+            child: child,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  CustomScrollView buildIosWidget(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        sliverAppBar,
+        CupertinoSliverRefreshControl(
+          onRefresh: onRefresh,
+        ),
+        SliverToBoxAdapter(child: child),
+      ],
+    );
+  }
+}
