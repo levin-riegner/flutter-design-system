85b6b254a28ebc20686cad7b6eff8f55175179b0
JTorrus <jtorrusgalindo@gmail.com>
2022-02-26 03:31:27 +0100

    Added padding to content placeholder views, alerts and adaptive tap behavior for list item action

--- lib/views/ds_list_item_action.dart
+++ lib/views/ds_list_item_action.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:lr_design_system/utils/dimens.dart';
 import 'package:lr_design_system/views/ds_divider.dart';
 
@@ -12,6 +13,8 @@ class DSListItemAction extends StatelessWidget {
   final Color? dividerColor;
   final Color? actionColor;
   final EdgeInsetsGeometry? dividerPadding;
+  final Widget? trailing;
+  final DSListItemActionTapBehavior tapBehavior;
 
   const DSListItemAction({
     required this.text,
@@ -23,51 +26,54 @@ class DSListItemAction extends StatelessWidget {
     this.dividerColor,
     this.actionColor,
     this.dividerPadding,
+    this.trailing,
+    this.tapBehavior = DSListItemActionTapBehavior.android,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Material(
-          color: Theme.of(context).colorScheme.background,
-          child: InkWell(
-            onTap: onPressed,
-            child: SizedBox(
-              width: double.infinity,
-              height: Dimens.of(context).listItemHeightLarge,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: Dimens.of(context).marginLarge),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    if (leading != null)
-                      Padding(
-                        padding: EdgeInsets.only(
-                            right: Dimens.of(context).marginSmall),
-                        child: leading,
-                      ),
-                    Expanded(
-                      child: Text(
-                        text,
-                        style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                              color: textColor ??
-                                  (type == DSListItemActionType.destructive
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Theme.of(context)
-                                          .colorScheme
-                                          .onBackground),
-                            ),
-                      ),
+        _AdaptiveTapAction(
+          tapBehavior: tapBehavior,
+          onPressed: onPressed,
+          child: SizedBox(
+            width: double.infinity,
+            height: Dimens.of(context).listItemHeightLarge,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: Dimens.of(context).marginLarge),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  if (leading != null)
+                    Padding(
+                      padding: EdgeInsets.only(
+                          right: Dimens.of(context).marginSmall),
+                      child: leading,
                     ),
-                    if (type == DSListItemActionType.navigation)
-                      Icon(
-                        Icons.chevron_right,
-                        color: actionColor ?? Theme.of(context).colorScheme.primary,
-                      ),
-                  ],
-                ),
+                  Expanded(
+                    child: Text(
+                      text,
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            color: textColor ??
+                                (type == DSListItemActionType.destructive
+                                    ? Theme.of(context).colorScheme.primary
+                                    : Theme.of(context)
+                                        .colorScheme
+                                        .onBackground),
+                          ),
+                    ),
+                  ),
+                  if (type == DSListItemActionType.navigation)
+                    trailing != null
+                        ? trailing!
+                        : Icon(
+                            Icons.chevron_right,
+                            color: actionColor ??
+                                Theme.of(context).colorScheme.primary,
+                          ),
+                ],
               ),
             ),
           ),
@@ -84,4 +90,40 @@ class DSListItemAction extends StatelessWidget {
   }
 }
 
+class _AdaptiveTapAction extends StatelessWidget {
+  final DSListItemActionTapBehavior tapBehavior;
+  final VoidCallback onPressed;
+  final Widget child;
+
+  const _AdaptiveTapAction({
+    Key? key,
+    required this.tapBehavior,
+    required this.onPressed,
+    required this.child,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    if (tapBehavior == DSListItemActionTapBehavior.android) {
+      return Material(
+        color: Theme.of(context).colorScheme.background,
+        child: InkWell(
+          onTap: onPressed,
+          child: child,
+        ),
+      );
+    } else {
+      return CupertinoButton(
+        padding: EdgeInsets.zero,
+        onPressed: onPressed,
+        child: child,
+      );
+    }
+  }
+}
+
 enum DSListItemActionType { navigation, destructive, simple }
+
+enum DSListItemActionTapBehavior { android, ios }
